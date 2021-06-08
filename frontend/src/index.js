0f1b895771bf3a5dc5dcde3c7fce83a8cdae2085
HuydDo <huydinhdo@yahoo.com>
2021-06-08 09:22:35 -0600

    modified index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// jsx includes html-like syntax
-const myelement = <h1><u>These are the details of the myelement const</u></h1>;
+// Call Random User Generator API
+const restEndpoint = 'https://randomuser.me/api/';
 
-// Where the magic happens!
+// Wait for response & output Let's try to output the API response in React!
+const callRestApi = async () => {
+  const response = await fetch(restEndpoint);
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
+
+  // React.createElement( type, [properties], [...children]);
+  return React.createElement('h1', null, JSON.stringify(jsonResponse));
+};
+
 ReactDOM.render(
-  myelement,
+  callRestApi(),
   document.querySelector('#root')
 );
